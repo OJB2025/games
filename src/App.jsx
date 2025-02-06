33e45d61c7df83ad18fec7530db118d748c3d73a
unknown <jajiolakunle@gmail.com>
2025-02-05 21:15:09 -0700

    I added reset button

--- src/App.jsx
+++ src/App.jsx
@@ -22,65 +22,34 @@ function App() {
       }                                    
     }
   }
+  
+  const resetGame = () => {
+    setBox1('')
+    setBox2('')
+    setBox3('')
+    setBox4('')
+    setBox5('')
+    setBox6('')
+    setBox7('')
+    setBox8('')
+    setBox9('')
+    setCurrentPlayer("X")
+  }
   return (
     <>
     <h2>tictactoe</h2>
       <div className='bigbox'>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box1, setBox1)
-          }
-          }
-          >{box1}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box2, setBox2)
-          }
-          }
-          >{box2}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box3, setBox3)
-          }
-          }
-          >{box3}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box4, setBox4)
-          }
-          }
-          >{box4}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box5, setBox5)
-          }
-          }
-          >{box5}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box6, setBox6)
-          }
-          }
-          >{box6}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box7, setBox7)
-          }
-          }
-          >{box7}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box8, setBox8)
-          }
-          }
-          >{box8}</div>
-          <div className='box'
-          onClick={()=>{
-            clickBox(box9, setBox9)
-          }
-          }
-          >{box9}</div>
+        <div className='box'onClick={()=>{clickBox(box1, setBox1)}}>{box1}</div>
+        <div className='box'onClick={()=>{clickBox(box2, setBox2)}}>{box2}</div>
+        <div className='box'onClick={()=>{clickBox(box3, setBox3)}}>{box3}</div>
+        <div className='box'onClick={()=>{clickBox(box4, setBox4)}}>{box4}</div>
+        <div className='box'onClick={()=>{clickBox(box5, setBox5)}}>{box5}</div>
+        <div className='box'onClick={()=>{clickBox(box6, setBox6)}}>{box6}</div>
+        <div className='box'onClick={()=>{clickBox(box7, setBox7)}}>{box7}</div>
+        <div className='box'onClick={()=>{clickBox(box8, setBox8)}}>{box8}</div>
+        <div className='box'onClick={()=>{clickBox(box9, setBox9)}}>{box9}</div>
       </div>
+      <button onClick={resetGame}>Reset Game</button> {/* Reset button */}
     </>
     
   )
